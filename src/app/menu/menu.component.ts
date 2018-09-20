23ee1a456504f09ee2e2fdb5c4939c6212d77e62
Rémi GALICHON <Remi.GALICHON@Equadex.net>
2018-09-20 13:27:21 +0200

    14. Display the user

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from '../user.service';
+import { UserModel } from '../models/user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'pr-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   navbarCollapsed = true;
+
+  user: UserModel;
+
+  userEventsSubscription: Subscription;
 
   toggleNavbar() {
     this.navbarCollapsed = !this.navbarCollapsed;
   }
 
   ngOnInit() {
+    this.userEventsSubscription = this.userService.userEvents.subscribe(e => this.user = e);
+  }
+
+  ngOnDestroy() {
+    if (this.userEventsSubscription) {
+      this.userEventsSubscription.unsubscribe();
+    }
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CredentialsModel } from './models/credentials.model';
+import { Observable, Subject } from 'rxjs';
+import { UserModel } from './models/user.model';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  public userEvents = new Subject<UserModel>();
 
   constructor(private http: HttpClient) { }
 
@@ -22,10 +25,9 @@ export class UserService {
     return this.http.post(`${baseUrl}/api/users`, body);
   }
 
-  authenticate(credentials: CredentialsModel): Observable<any> {
-
-    const baseUrl = 'http://ponyracer.ninja-squad.com';
-
-    return this.http.post(`${baseUrl}/api/users/authentication`, credentials);
+  authenticate(credentials: { login: string; password: string }): Observable<UserModel> {
+    return this.http.post<UserModel>('http://ponyracer.ninja-squad.com/api/users/authentication', credentials).pipe(
+      tap((user: UserModel) => this.userEvents.next(user))
+    );
   }
 }
